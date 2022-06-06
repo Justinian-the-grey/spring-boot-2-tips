b7ae58666cb7c501ed891ecb7924c45ed921cf35
Justinian <nicky_torvald_dk@protonmail.com>
2022-06-06 16:45:14 +0200

    initial commit

---
+++ src/main/java/com/example/demo/SpringBootTipsPart1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTipsPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTipsPart1Application.class, args);
+	}
+
+}
